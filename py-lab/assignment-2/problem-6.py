31f0776e09a06d31f66898158474679ea8632a68
Sk Sameer Salam <113439449+its-triple-s@users.noreply.github.com>
2023-08-25 16:21:11 +0530

    Added question as comment.

--- py-lab/assignment-2/problem-6.py
+++ py-lab/assignment-2/problem-6.py
@@ -1,3 +1,5 @@
+# Write a program to check whether a character is a vowel or a consonant.
+
 letter = input("Character: ").lower()
 
 if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
