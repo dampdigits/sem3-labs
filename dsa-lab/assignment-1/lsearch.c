14376fce97a3c7d40d6642fa429631747393f4f3
Sk Sameer Salam <113439449+its-triple-s@users.noreply.github.com>
2023-09-02 17:04:18 +0530

    added files

---
+++ dsa-lab/assignment-1/lsearch.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+  
+void main()
+{
+    int a[20],i,x,n;
+    //clrscr();
+    printf("How many array elements?\n");
+    scanf("%d",&n);
+     
+    printf("Enter array elements\n");
+    for(i=0;i<n;++i)
+        scanf("%d",&a[i]);
+     
+    printf("Enter element to search:\n");
+    scanf("%d",&x);
+     
+    for(i=0;i<n;++i)
+        if(a[i]==x)
+            break;
+     
+    if(i<n)
+        printf("Element found at index=%d",i);
+    else
+        printf("Element not found");
+  
+    getch();
+}
