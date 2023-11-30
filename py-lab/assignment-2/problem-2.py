540dea89e01b529d5f6be9c4acd0532ec7c35390
Sk Sameer Salam <113439449+dampdigits@users.noreply.github.com>
2023-11-30 17:46:03 +0000

    added output

--- py-lab/assignment-2/problem-2.py
+++ py-lab/assignment-2/problem-2.py
@@ -10,3 +10,11 @@ coins1 = int(input("₹ 1: "))
 money = (10 * coins10) + (5 * coins5) + (2 * coins2) + (1 * coins1)
 
 print(f"Money: {money}")
+
+# Output:
+# Enter the number coins of:
+# ₹ 10: 5
+# ₹ 5: 6
+# ₹ 2: 2
+# ₹ 1: 0
+# Money: 84
