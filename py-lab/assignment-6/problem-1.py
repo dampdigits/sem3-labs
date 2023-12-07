442256650446a27e2a25e60422111966fd1ea50a
Sk Sameer Salam <113439449+dampdigits@users.noreply.github.com>
2023-12-07 17:58:15 +0000

    syntax update

--- py-lab/assignment-6/problem-1.py
+++ py-lab/assignment-6/problem-1.py
@@ -1,18 +1,29 @@
+"""Program to illustrate OOP"""
+
+from datetime import date
+
 class Person:
-    def _init_(self, name, country, date_of_birth):
+    """Class representing a person"""
+
+    def __init__(self, name, country, date_of_birth):
         self.name = name
         self.country = country
         self.date_of_birth = date_of_birth
     def calculate_age(self):
+        """Function to calculate age"""
+
         today = date.today()
         age = today.year - self.date_of_birth.year
         if today < date(today.year, self.date_of_birth.month, self.date_of_birth.day):
             age -= 1
             return age
+        return None
+
 # Example usage
 person1 = Person("Ferdi Odilia", "France", date(1962, 7, 12))
 person2 = Person("Shweta Maddox", "Canada", date(1982, 10, 20))
 person3 = Person("Elizaveta Tilman", "USA", date(2000, 1, 1))
+
 # Accessing attributes and calculating age
 print("Person 1:")
 print("Name:", person1.name)
